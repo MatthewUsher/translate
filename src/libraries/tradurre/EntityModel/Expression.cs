bdfe158c9fe66fb0d26ec7c527e9fcb6efa25edc
Matt Usher <matt@usher.casa>
2025-06-06 12:09:25 -0700

    Adding in the base fragment and SQL standard data types

---
+++ src/libraries/tradurre/EntityModel/Expression.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="Expression.cs">
+// All rights reserved.
+// </copyright>
+
+namespace Tradurre;
+
+/// <summary>
+/// Represents a SQL expression.
+/// </summary>
+public abstract class Expression : Fragment
+{
+}
