0df25045e637f1613485815b7559684ee12accc2
Matt Usher <two_under@hotmail.com>
2025-06-06 18:26:21 -0700

    Dev (#5)
    * Adding the base build.yml
    
    * Adding the base translate library
    
    * Adding the base error, warning, and source to the translate library
    
    * Fixing a typo in the README.
    
    * Adding in the base fragment and SQL standard data types
    
    * Adding the base tests for the Tradurre library
    
    * Adding the SQL Server data types
    
    ---------
    
    Co-authored-by: Matt Usher <matt@usher.casa>

---
+++ src/libraries/tradurre/EntityModel/DataTypes/BitDataType.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="BitDataType.cs">
+// All rights reserved.
+// </copyright>
+
+namespace Tradurre;
+
+/// <summary>
+/// Represents a BIT data type.
+/// </summary>
+/// <remarks>
+/// Microsoft SQL Server: <see href="https://learn.microsoft.com/en-us/sql/t-sql/data-types/bit-transact-sql?view=sql-server-ver17"/>
+/// PostgreSQL: <see href="" />
+/// </remarks>
+public sealed class BitDataType : DataType
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BitDataType"/> class.
+    /// </summary>
+    public BitDataType()
+        : base()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BitDataType"/> class with the specified <see cref="Source"/>.
+    /// </summary>
+    /// <param name="source">A <see cref="Source"/>.</param>
+    public BitDataType(Source source)
+        : base(source)
+    {
+    }
+}
---
+++ tests/TradurreTests/SourceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tradurre;
+
+[TestClass]
+public sealed class SourceTests
+{
+    [TestMethod]
+    public void ToJsonTest()
+    {
+        Source s = new();
+
+        string expected = "{\"FileName\":null,\"LineNumber\":-1,\"Value\":\"\"}";
+
+#if DEBUG
+        expected = "{\r\n  \"FileName\": null,\r\n  \"LineNumber\": -1,\r\n  \"Value\": \"\"\r\n}";
+#endif
+
+        Assert.AreEqual(expected, s.ToJson());
+    }
+}
